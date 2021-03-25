90c553f2da7a8b33bea2846228b7115259c61b01
bin <vincent418@icloud.com>
2021-03-24 22:20:02 -0500

    commit tree 166a83286b97cf58b87e0efd15d038ab811153a1

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// const num1 = 8;
+//     num2 = 7.75;
+// const mutiply(number) {
+//     return num1 * num2;
+// }
+// multiple(number)
+const num1 = 8
+const num2 = 7.75
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 10) + 1;
+const num3 = 67
+const num4 = 7
+const mod = num3 % num4
+const max = 20
 
-// Write your code here
